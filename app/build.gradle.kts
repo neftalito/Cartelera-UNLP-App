0e0d240b84e999ea85c31e711fe25ce603bdfea6
neftalito <47830827+neftalito@users.noreply.github.com>
2026-07-05 13:08:12 -0300

    Incremento la versión de la build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.overcoders.unlpcarteleranotifier"
         minSdk = 23
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.2.0"
+        versionCode = 20
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
