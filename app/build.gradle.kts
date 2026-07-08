9ecacbc4edcc19121cb946e45fd5399a3971324a
neftalito <47830827+neftalito@users.noreply.github.com>
2026-07-08 12:59:25 -0300

    Incremento la versión de la build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         minSdk = 23
         //noinspection OldTargetApi
         targetSdk = 36
-        versionCode = 21
-        versionName = "2.0.1"
+        versionCode = 22
+        versionName = "2.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
